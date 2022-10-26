f934ff05fbd499cee4321c314f96f7e26fa9eaa6
Espagnol Thibault <thibault.espagnol@epita.fr>
2022-10-26 20:57:09 +0200

    add RotationString.php

--- LevelC/RotationString.php
+++ LevelC/RotationString.php
@@ -13,10 +13,15 @@ class RotationString
      * @return bool|int
      */
     public static function isRotation($s1, $s2)
-    {
-        /** @TODO */
-
-        return false;
+    { 
+        if (strlen($s1) !== strlen($s2)) {
+            return false;
+        }
+        else
+        {
+            $s1 = $s1 . $s1;
+            return strpos($s1, $s2);
+        }
     }
 
     public static function isSubString($s1, $s2)
