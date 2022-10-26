0c561d6a53a5cf247edc759b868c0eff68d78855
Espagnol Thibault <thibault.espagnol@epita.fr>
2022-10-26 21:50:34 +0200

    add A and F

--- LevelA/Palindrome.php
+++ LevelA/Palindrome.php
@@ -14,15 +14,42 @@ class Palindrome
     /**
      * This function creates a palindrome with his $str attributes
      * If $str is abc then this function return abccba
-     *
+     * 
      * @TODO
      * @return string
      */
+    public function revert(string $input) : string {
+        // get all characters; asuming ascii
+        $chars = [];
+        for($i = 0; $i < strlen($input); $i++) {
+            if (ctype_alpha($input[$i])) {
+                $chars[] = $input[$i];
+            }
+        }
+        
+        // replace characters
+        for($i = 0;$i < strlen($input); $i++) {
+            if (ctype_alpha($input[$i])) {
+                $input[$i] = array_pop($chars);
+            }
+        }
+        
+        return $input;
+    }
     public function generatePalindrome()
     {
         /** @TODO */
-
-        return 'abccba';
+    
+        $res = "";
+        for ($i=0; $i < strlen($this->str); $i++) {
+            if ($this->str[$i] == '\\'){
+                $res = $this->str[$i] . $this->str[$i+1]. $res;
+                $i = $i + 1;
+            }
+            else
+                $res = $this->str[$i] . $res;
+        }
+        return $this->str . $res;
+        // return $this->str . $this->revert($this->str);
     }
-
 }
